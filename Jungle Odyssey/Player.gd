8f3669f18a3077cbdc4b2848776f284a30f198f5
carder46 <100096994+carder46@users.noreply.github.com>
2024-05-20 09:34:59 +1200

    fixed animation

--- Jungle Odyssey/Player.gd
+++ Jungle Odyssey/Player.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var direction : Vector2 = Vector2.ZERO
+var swing : bool = false
+
+@onready var animation_tree = $AnimationTree
+
+
 
 
 func _physics_process(delta):
@@ -16,12 +23,23 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		set_running(true)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		set_running(false)
 	move_and_slide()
+
+func _process(delta):
+	if Input.is_action_pressed("Right"):
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("Left"):
+		$AnimatedSprite2D.flip_h = true
+
+func set_running(val):
+	animation_tree["parameters/conditions/is_running"] = val
+	animation_tree["parameters/conditions/idle"] = not val
+	
