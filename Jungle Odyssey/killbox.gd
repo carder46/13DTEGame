11d64aca0b378fcdb2c2eac32f2cd1d6489f170b
carder46 <100096994+carder46@users.noreply.github.com>
2024-06-14 09:58:57 +1200

    period 1 on friday, alot done

--- Jungle Odyssey/killbox.gd
+++ Jungle Odyssey/killbox.gd
@@ -8,5 +8,5 @@ func _ready():
 
 func _on_area_entered(area):
 	if area.is_in_group("player_kill_detect"):
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://lose.tscn")
 
--- Jungle Odyssey/start.gd
+++ Jungle Odyssey/start.gd
@@ -9,6 +9,5 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://character_selection.tscn")
 
 
-
 func _on_quit_pressed():
 	get_tree().quit()
